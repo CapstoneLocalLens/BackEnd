6c0823aac785564f74182e9b04ba05f824012163
EgPwl <egpwl07@gmail.com>
2024-11-13 15:14:27 +0900

    [feat]: 시간대별 유동인구 controller

--- src/main/java/com/example/localens/analysis/controller/PopulationController.java
+++ src/main/java/com/example/localens/analysis/controller/PopulationController.java
@@ -1,6 +1,7 @@
 package com.example.localens.analysis.controller;
 
 import com.example.localens.analysis.service.GenderAge;
+import com.example.localens.analysis.service.TimeZoneFlow;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -15,10 +16,18 @@ public class PopulationController {
     @Autowired
     private GenderAge genderAge;
 
+    @Autowired
+    private TimeZoneFlow timeZoneFlow;
+
 
     @GetMapping("/gender-age-ratio")
     public Map<String, Map<String, Double>> getGenderAgeRatio() {
         return genderAge.getGenderAgeRatios();
+    }
+
+    @GetMapping("/time-zone-ratio")
+    public Map<String, Double> getTimeZoneRatio() {
+        return timeZoneFlow.getTimeZoneRatio();
     }
 
 }
